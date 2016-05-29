223bfadddabb6b3669b2ef625d221c3c6979dcba
NotSpecial <alexander@dietmueller.de>
2016-05-29 21:05:02 +0200

    Added form to adjust yearly settings, refactored html a lot
    Using lots of include the main templates is now broken down into little parts.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@
 
 import os
 import shutil
-import json
+import yaml
 import logging
 from datetime import datetime
 from locale import setlocale, LC_TIME
@@ -12,7 +12,9 @@ from locale import setlocale, LC_TIME
 from flask import (Flask, render_template, send_file, redirect, url_for,
                    session, request)
 from flask_wtf import Form
-from wtforms import StringField, IntegerField, FormField, DateField
+from wtforms import StringField, TextAreaField, FormField
+from wtforms.validators import InputRequired
+from wtforms.fields.html5 import IntegerField, DateField
 
 from contractor.tex import render_tex
 from contractor.soapclient import CRMImporter
@@ -20,48 +22,57 @@ from contractor.api_auth import api_auth, protected
 
 # Set locale to ensure german weekdays
 setlocale(LC_TIME, "de_CH.UTF-8")
-dateformat = "%d.%m.%Y"
+dateformat = "%Y-%m-%d"
+dateplaceholder = "yyyy-mm-dd"
 
 
 app = Flask('contractor')
 
 app.config.from_pyfile('config.py')
 
-if not app.debug and app.config.get('ENABLE_LOG', False):
+if 'LOG_DIR' in app.config:
     logpath = os.path.join(app.config['LOG_DIR'],
                            'flask.log')
     file_handler = logging.FileHandler(logpath)
     file_handler.setLevel(app.config['LOGLEVEL'])
     app.logger.addHandler(file_handler)
 
-app.config['DESCRIPTIONS'] = {
-    'html': {
-        # booth descriptions for html
-        # small booths
-        'sA1': 'small, cat A, one day',
-        'sA2': 'small, cat A, two days',
-        'sB1': 'small, cat B, one day',
-        'sB2': 'small, cat B, two days',
-        # big booths,
-        'bA1': 'big, cat A, one day',
-        'bA2': 'big, cat A, two days',
-        'bB1': 'big, cat B, one day',
-        'bB2': 'big, cat B, two days',
-        # startops,
-        'su1': r'\startupone',
-        'su2': r'\startuptwo'
-    }
+# All different options in neat order (used to create form)
+choices = ['sA1', 'sA2', 'sB1', 'sB2',
+           'bA1', 'bA2', 'bB1', 'bB2',
+           'su1', 'su2',
+           'media', 'business', 'first']
+
+# Additional fulltext
+fulltext = {
+    # small booths
+    'sA1': 'small, cat A, one day',
+    'sA2': 'small, cat A, two days',
+    'sB1': 'small, cat B, one day',
+    'sB2': 'small, cat B, two days',
+    # big booths,
+    'bA1': 'big, cat A, one day',
+    'bA2': 'big, cat A, two days',
+    'bB1': 'big, cat B, one day',
+    'bB2': 'big, cat B, two days',
+    # startups,
+    'su1': 'start-up, one day',
+    'su2': 'start-up, two days',
+    # packets
+    'media': 'media packet',
+    'business': 'business packet',
+    'first': 'first packet'
 }
 
 app.config['YEARLY_SETTINGS'] = {
     'fairtitle': 'AMIV Kontakt.16',
     'president': 'Alexander Ens',
-    'sender': 'Pascal Gutzwiller',
+    'sender': 'Pascal Gutzwiller\nQuästor Kommission Kontakt',
 
     # Fair days,
     'days': {
-        'first': datetime.strptime('18.10.2016', dateformat).date(),
-        'second': datetime.strptime('19.10.2016', dateformat).date(),
+        'first': datetime.strptime('18.10.2016', "%d.%m.%Y").date(),
+        'second': datetime.strptime('19.10.2016', "%d.%m.%Y").date(),
     },
 
     # Prices, all in francs
@@ -83,49 +94,61 @@ app.config['YEARLY_SETTINGS'] = {
 }
 
 
+class PriceField(IntegerField):
+    """Special field that can be recognized in the templated for formatting."""
+
+    pass
+
+
 class PricesSubForm(Form):
-    """Form for prices, will be part of main form."""
+    """Form for prices, will be part of main form.
 
-    # small
-    sA1 = IntegerField(render_kw={'placeholder': '1234'})
-    sA2 = IntegerField(render_kw={'placeholder': '1234'})
-    sB1 = IntegerField(render_kw={'placeholder': '1234'})
-    sB2 = IntegerField(render_kw={'placeholder': '1234'})
+    Empty form so far, we will add the attributes in a loop since they are
+    many and all look the same.
+    """
 
-    # big
-    bA1 = IntegerField(render_kw={'placeholder': '1234'})
-    bA2 = IntegerField(render_kw={'placeholder': '1234'})
-    bB1 = IntegerField(render_kw={'placeholder': '1234'})
-    bB2 = IntegerField(render_kw={'placeholder': '1234'})
+    pass
 
-    # startup
-    su1 = IntegerField(render_kw={'placeholder': '1234'})
-    su2 = IntegerField(render_kw={'placeholder': '1234'})
-
-    # packets
-    first = IntegerField(render_kw={'placeholder': '1234'})
-    business = IntegerField(render_kw={'placeholder': '1234'})
-    media = IntegerField(render_kw={'placeholder': '1234'})
+for shortname in choices:
+    setattr(PricesSubForm,
+            shortname,
+            PriceField(label=fulltext[shortname],
+                       validators=[InputRequired()],
+                       render_kw={'placeholder': '1234'}))
 
 
 class DaysSubForm(Form):
     """Form for days, will be part of main form."""
 
-    first = DateField(format=dateformat,
-                      render_kw={'placeholder': 'dd.mm.yyyy'})
-    second = DateField(format=dateformat,
-                       render_kw={'placeholder': 'dd.mm.yyyy'})
+    first = DateField(label='First Day of Fair',
+                      format=dateformat,
+                      validators=[InputRequired()],
+                      render_kw={'placeholder': dateplaceholder})
+    second = DateField(label='Second Day of Fair',
+                       format=dateformat,
+                       validators=[InputRequired()],
+                       render_kw={'placeholder': dateplaceholder})
 
 
 class YearlySettingsForm(Form):
     """Form to change yearly settings for the fair."""
 
-    fairtitle = StringField('fairtitle',
+    fairtitle = StringField(label='Title of Fair',
+                            validators=[InputRequired()],
                             render_kw={'placeholder': 'Kontakt.YY'})
-    president = StringField('president',
-                            render_kw={'placeholder': 'firstname lastname'})
-    sender = StringField('sender',
-                         render_kw={'placeholder': 'firstname lastname'})
+    president = StringField(label='President',
+                            validators=[InputRequired()],
+                            description='The president of the Kontakt '
+                                        'Kommission',
+                            render_kw={'placeholder': 'Firstname Lastname'})
+    sender = TextAreaField(label='Sender',
+                           validators=[InputRequired()],
+                           description='Sender of contract letters, usually '
+                                       'the president or treasurer. You can '
+                                       'use several lines to add title etc.',
+                           render_kw={
+                               'placeholder': 'Firstname Lastname\n'
+                                              '(Additional info, if any)'})
 
     prices = FormField(PricesSubForm)
     days = FormField(DaysSubForm)
@@ -146,7 +169,7 @@ app.config['APIAUTH_URL'] = 'https://nicco.io/amiv/'
 
 
 # Import companies
-def _storagepath(name):
+def _datapath(name):
         return os.path.join(app.config['DATA_DIR'], name)
 
 
@@ -158,24 +181,32 @@ def _refresh_companies():
     app.config['ERRORS'] = errors
 
     # Store
-    with open(_storagepath('letterdata.json'), 'w') as f:
-        json.dump(letterdata, f)
+    with open(_datapath('letterdata.yml'), 'w') as f:
+        yaml.dump(letterdata, f)
 
-    with open(_storagepath('errors.json'), 'w') as f:
-        json.dump(errors, f)
+    with open(_datapath('errors.yml'), 'w') as f:
+        yaml.dump(errors, f)
 
-    # Remove everything in storage and recreate dir
+    # Remove everything in storage (finished contracts) and recreate dir
     shutil.rmtree(app.config['STORAGE_DIR'])
     os.mkdir(app.config['STORAGE_DIR'])
 
 # Load on startup
 try:
-    with open(_storagepath('letterdata.json'), 'r') as f:
-        app.config['LETTERDATA'] = json.load(f)
-    with open(_storagepath('errors.json'), 'r') as f:
-        app.config['ERRORS'] = json.load(f)
+    with open(_datapath('letterdata.yml'), 'r') as f:
+        app.config['LETTERDATA'] = yaml.load(f)
+    with open(_datapath('errors.yml'), 'r') as f:
+        app.config['ERRORS'] = yaml.load(f)
 except OSError:
     _refresh_companies()
+
+# if there are any yearly settings saved, overwrite defaults
+try:
+    with open(_datapath('yearly_settings.yml'), 'r') as f:
+        app.config['YEARLY_SETTINGS'] = yaml.load(f)
+except OSError:
+    # Keep defaults
+    pass
 
 
 # Routes
@@ -195,11 +226,18 @@ def main():
     # Form for yearly settings
     settings_form = YearlySettingsForm(**app.config['YEARLY_SETTINGS'])
 
+    if settings_form.validate_on_submit():
+        print("yehaa")
+        # Adjust settings and save them to file
+        app.config['YEARLY_SETTINGS'] = settings_form.data
+        with open(_datapath('yearly_settings.yml'), 'w') as f:
+            yaml.dump(app.config['YEARLY_SETTINGS'], f)
+
     return render_template('main.html',
                            user=session['logged_in'],
                            output_format=session.get('output_format', 'mail'),
                            settings_form=settings_form,
-                           descriptions=app.config['DESCRIPTIONS']['html'],
+                           descriptions=fulltext,
                            companies=app.config['LETTERDATA'],
                            errors=app.config['ERRORS'])
 
