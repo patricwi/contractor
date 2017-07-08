0f914111273a183f1859ad34b116e667b6c92091
Alexander Dietmüller <alexander@dietmueller.de>
2017-07-08 04:31:55 +0200

    Removed no longer needed settings

--- contractor/settings.py
+++ contractor/settings.py
@@ -2,10 +2,5 @@
 
 """App settings."""
 
-# All SOAP settings except password
-SOAP_URL = "https://people.ee.ethz.ch/~amivkt/crm/service/v2/soap.php?wsdl"
-SOAP_APPNAME = "AMIV Kontakt: Internal: Customer Relationship Management"
-SOAP_USERNAME = "soap"
-
 # URL for amivapi
 AMIVAPI_URL = 'https://amiv-api.ethz.ch/'
